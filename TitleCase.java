119eeb1a5791a971eaf3719ece46483caf1b408f
release_the_kraken <wmichal13@wp.pl>
2022-04-01 10:14:34 +0200

    Changed the TitleCase class to title case multiple word strings

--- TitleCase.java
+++ TitleCase.java
@@ -1,7 +1,11 @@
 public class TitleCase {
 //changing the first letter of a word to upper case
     String titleCase(String str){
-        String firstLetter = "" + str.charAt(0);
-        return str.replace(firstLetter, firstLetter.toUpperCase());
+        String[] arrayFromStr = str.split(" ");
+        for(int i = 0; i < arrayFromStr.length; i++){
+            String firstLetter = "" + arrayFromStr[i].charAt(0);
+            arrayFromStr[i] = arrayFromStr[i].replaceFirst(firstLetter, firstLetter.toUpperCase());
+        }
+        return String.join(" ", arrayFromStr);
     }
 }
